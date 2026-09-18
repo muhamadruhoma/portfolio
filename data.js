84281730ae8cbdb9cdc9c4793f213994637ea0ee
Muhamad Makram <197615212+muhamadruhoma@users.noreply.github.com>
2026-09-18 22:42:45 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,30 @@
+
+const works = [
+  {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Emad Afandy Coffee.",ar:"إعلان غير رسمي ل قهوة عماد أفندي"},image:"assets/works/ads/emadafandy.png"},
+  {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Spero Spathis apple.",ar:"إعلان غير رسمي لسبيرو سباتس بالتفاح"},image:"assets/works/ads/speroapple.png"},
+  {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Spero Spathis Cherry.",ar:"إعلان غير رسمي لسبيرو سباتس بالتوت"},image:"assets/works/ads/sperocherry.png"},
+  {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Dairy Milk.",ar:"إعلان غير رسمي لديري ميلك"},image:"assets/works/ads/dairymilk.png"},
+  {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Spuds.",ar:"إعلان غير رسمي لسبدز"},image:"assets/works/ads/spuds.png"},
+  {category:"Thumbnail",title:{en:"Thumbnail",ar:"صورة مصغرة"},description:{en:"Thumbnail for a YT video.",ar:"صورة مصغرة لفيديو على يوتيوب"},image:"assets/works/thumbnails/dev1.png"},
+  {category:"Thumbnail",title:{en:"Thumbnail",ar:"صورة مصغرة"},description:{en:"Thumbnail for a YT video.",ar:"صورة مصغرة لفيديو على يوتيوب"},image:"assets/works/thumbnails/art1.png"},
+  {category:"Thumbnail",title:{en:"Thumbnail",ar:"صورة مصغرة"},description:{en:"Thumbnail for a YT video.",ar:"صورة مصغرة لفيديو على يوتيوب"},image:"assets/works/thumbnails/mc1.png"},
+  {category:"Digital Art",title:{en:"Digital Art",ar:"الرسم الرقمي"},description:{en:"Digital artwork.",ar:"عمل رسم رقمي."},image:"assets/works/pp/mu.png"},
+  {category:"Design",title:{en:"Profile picture"},description:{en:"Profile picture.",ar:"صورة ملف الشخصي."},image:"assets/works/pp/itsmuday.png"},
+  {category:"Design",title:{en:"Logo Design",ar:"تصميم الشعار"},description:{en:"Branding and visual design.",ar:"مشروع هوية وتصميم بصري."},image:"assets/works/pp/thecube.png"},
+  {category:"Banner",title:{en:"MC YouTube Banner",ar:"بانر قناة يوتيوب"},description:{en:"A banner for a YouTube channel.",ar:"بانر لقناة على يوتيوب."},image:"assets/works/banners/banner4.png"},
+  {category:"Banner",title:{en:"YouTube Banner",ar:"بانر قناة يوتيوب"},description:{en:"A banner for a YouTube channel.",ar:"بانر لقناة على يوتيوب."},image:"assets/works/banners/banner2.jpg"},
+  {category:"Game Dev",title:{en:"Game Asset",ar:"اسيت للعبة"},description:{en:"Pixel art gaming asset.",ar:"اسيت للعبة على بستايل بكسل"},image:"assets/works/pixel/pixel.jpg"},
+  {category:"Minecraft",title:{en:"Custom Posing",ar:"وضعيات مخصصة"},description:{en:"Custom posing for Minecraft skins.",ar:"وضعيات مخصصة لسكنات ماين كرافت."},image:"assets/works/mcposes/mcposes.jpg"},
+  {category:"MORE",title:{en:"SOON",ar:"قريبا"}},
+];
+
+const services = [
+  {title:{en:"Digital Art & Illustration",ar:"الرسم والفن الرقمي"},price:{en:"$Custom",ar:"حسب العمل$"}},
+  {title:{en:"Branding & Brand Kits",ar:"الهوية البصرية"},price:{en:"$Custom",ar:"حسب المطلوب$"}},
+  {title:{en:"Thumbnails & Social Design",ar:"الثمبنيل وتصاميم السوشيال"},price:{en:"$Custom",ar:"حسب التصميم$"}},
+  {title:{en:"Video Editing",ar:"مونتاج الفيديو"},price:{en:"$Custom",ar:"حسب الفيديو$"}},
+  {title:{en:"Game Art & Assets",ar:"رسومات وأسيت الألعاب"},price:{en:"$Custom",ar:"حسب المشروع$"}},
+  {title:{en:"Even More!",ar:"وأشياء أخرى!"},price:{en:"$Custom",ar:"حسب المشروع"}}
+];
+
+const siteConfig = { whatsapp:"+201069082675" };
