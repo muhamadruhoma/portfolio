a4b90e99735cff78bb854798ae6f5d776567c24f
Muhamad Makram <197615212+muhamadruhoma@users.noreply.github.com>
2026-09-20 15:16:33 +0300

    Add new social media post for Neurology Dr.

--- data.js
+++ data.js
@@ -1,6 +1,7 @@
 
 const works = [
   {category:"ٍSocial Media",title:{en:"Social media post",ar:"بوست سوشيال ميديا"},description:{en:"Social media post for Nasa Space Apps Challenge.",ar:"بوست سوشيال ميديا لمبادرة في ناسا"},image:"assets/works/ads/nasa.jpg"},
+  {category:"ٍSocial Media",title:{en:"Social media post",ar:"بوست سوشيال ميديا"},description:{en:"Social media post for Neurology Dr.",ar:"بوست سوشيال ميديا لدتور مخ وأعصاب"},image:"assets/works/ads/doctor.jpg"},
   {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Emad Afandy Coffee.",ar:"إعلان غير رسمي ل قهوة عماد أفندي"},image:"assets/works/ads/emadafandy.png"},
   {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Spero Spathis apple.",ar:"إعلان غير رسمي لسبيرو سباتس بالتفاح"},image:"assets/works/ads/speroapple.png"},
   {category:"Ads",title:{en:"Advertisement",ar:"إعلان"},description:{en:"Non-official ad for Spero Spathis Cherry.",ar:"إعلان غير رسمي لسبيرو سباتس بالتوت"},image:"assets/works/ads/sperocherry.png"},
